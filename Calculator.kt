fd27ad2423ceaa39bbf93e930b6a5df5e5856046
Gabriel Muñoz <93497614+mrGoonies@users.noreply.github.com>
2022-01-28 20:16:29 -0300

    Modificando App

--- Calculator.kt
+++ Calculator.kt
@@ -1,22 +1,99 @@
-class Calculator(name: String) {
-  init {
-    println("Bienvenido $name")
-  }
+import java.util.Scanner;
+import kotlin.test.assertFailsWith
 
-  fun add(num1: Int, num2: Int) = println("${num1 + num2}")
-  fun subtract(num1: Int, num2: Int) = println("${num1 - num2}")
-  fun multiply(num1: Int, num2: Int) = println("${num1 * num2}")
-  fun divide(num1: Int, num2: Int) = println("${num1 / num2}")
+// Instanciamos Scanner
+val scanner = Scanner(System.`in`)
+
+class Calculator(name: String) {
+
+    init {
+        println("Bienvenido $name.")
+    }
+
+    // Métodos
+    fun add(num1: Int, num2: Int) {
+        println("El resultado de la suma es:\n${num1 + num2}")
+    }
+
+    fun subtract(num1: Int, num2: Int) {
+        println("El resultado de la resta es:\n${num1 - num2}")
+    }
+
+    fun multiply(num1: Int, num2: Int) {
+        println("El resultado de la multiplicación es:\n${num1 * num2}")
+    }
+
+    fun divide(num1: Int, num2: Int) {
+        println("El resultado de la división es:\n${num1 / num2}")
+    }
 
 }
+
+// Solicitamos el nombre del usuario
+fun nameRequest(): String {
+    var input: String
+
+    println("Ingrese su nombre")
+    input = readLine().toString()
+
+    return input
+}
+
+// Recibimos los números que utilizará el usuario
+fun userNumber(): Int {
+    println("Debes ingresar un número para que nuestra cálculadora pueda trabajar")
+
+    var num = scanner.nextInt()
+
+    return num
+}
+
+// Generamos el menú de nuestra aplicación:
+fun menuApplication(): Int {
+    var input: Int
+    while (true) {
+        println("*** Menú ***\n1) Sumar\n2) Restar\n3) Multiplicación\n4) División\n5) Salir")
+        println("Ingrese una opción")
+
+        input = scanner.nextInt()
+
+        if (input > 0 && input < 6) {
+            println("haz seleccionado una opción")
+            break
+        } else{
+            continue
+        }
+    }
+
+    return input
+}
+
 
 fun main() {
-  val myCalculator = Calculator("Goonies")
+    val name = nameRequest()
+    val calculator = Calculator(name)
 
-  myCalculator.add(2, 5)
-  myCalculator.subtract(10, 20)
-  myCalculator.multiply(5, 5)
-  myCalculator.divide(100, 58)
+    // Almacenamos los números del usuario
+    var numA = userNumber()
+    var numB = userNumber()
+    var optionMenu = menuApplication()
 
-  
+    when(optionMenu) {
+        1 -> calculator.add(numA, numB)
+        2 -> calculator.subtract(numA, numB)
+        3 -> calculator.multiply(numA, numB)
+        4 -> calculator.divide(numA, numB)
+        5 -> println("Muchas gracias por probar esta aplicación")
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
